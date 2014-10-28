766be28737cfdb23b8a54952d470559a412b7808
Mike Calvanese <mfcalvanese@gmail.com>
2014-10-27 22:36:56 -0400

    Added tests for different apiRoot option values

--- test/initialization-spec.js
+++ test/initialization-spec.js
@@ -1,48 +1,87 @@
 describe('initialization', function() {
 
-    var appMock = {
-        use: function() {},
-        get: function() {},
-        post: function() {},
-        put: function() {},
-        delete: function() {}
-    };
+    var appMock, modelMock, k;
 
-    var modelMock = helpers.getModelMock('items');
-
-    var k;
+    beforeEach(function() {
+        appMock = helpers.getAppMock();
+        modelMock = helpers.getModelMock('items');
+        spyOn(appMock, 'get');
+        spyOn(appMock, 'post');
+        spyOn(appMock, 'put');
+        spyOn(appMock, 'delete');
+    })
 
     describe('with no options', function() {
 
         beforeEach(function() {
-            spyOn(appMock, 'get');
-            spyOn(appMock, 'post');
-            spyOn(appMock, 'put');
-            spyOn(appMock, 'delete');
             k = require('../index')(appMock);
             k.expose(modelMock);
         });
 
+        runEndpointConfigTests('/items');
+
+    });
+
+    describe('with an apiRoot option', function() {
+
+        beforeEach(function() {
+            k = require('../index')(appMock, { apiRoot: 'api' });
+            k.expose(modelMock);
+        });
+
+        runEndpointConfigTests('/api/items');
+
+    });
+
+    describe('with an apiRoot option that has more than one path segment',
+    function() {
+
+        beforeEach(function() {
+            k = require('../index')(appMock, { apiRoot: 'api/v1' });
+            k.expose(modelMock);
+        });
+
+        runEndpointConfigTests('/api/v1/items');
+
+    });
+
+    describe('with an apiRoot option that has a leading and trailing slash',
+    function() {
+
+        beforeEach(function() {
+            k = require('../index')(appMock, { apiRoot: '/api/' });
+            k.expose(modelMock);
+        });
+
+        runEndpointConfigTests('/api/items');
+
+    });
+
+    function runEndpointConfigTests(endpointPath) {
+
         it('should configure get endpoint for collection', function() {
-            expect(appMock.get.calls.argsFor(0)[0]).toEqual('/items');
+            expect(appMock.get.calls.argsFor(0)[0]).toEqual(endpointPath);
         });
 
         it('should configure get endpoint for single item', function() {
-            expect(appMock.get.calls.argsFor(1)[0]).toEqual('/items/:identifier');
+            expect(appMock.get.calls.argsFor(1)[0])
+                .toEqual(endpointPath + '/:identifier');
         });
 
         it('should configure post endpoint for collection', function() {
-            expect(appMock.post.calls.argsFor(0)[0]).toEqual('/items');
+            expect(appMock.post.calls.argsFor(0)[0]).toEqual(endpointPath);
         });
 
         it('should configure put endpoint for single item', function() {
-            expect(appMock.put.calls.argsFor(0)[0]).toEqual('/items/:identifier');
+            expect(appMock.put.calls.argsFor(0)[0])
+                .toEqual(endpointPath + '/:identifier');
         });
 
         it('should configure delete endpoint for single item', function() {
-            expect(appMock.delete.calls.argsFor(0)[0]).toEqual('/items/:identifier');
+            expect(appMock.delete.calls.argsFor(0)[0])
+                .toEqual(endpointPath + '/:identifier');
         });
 
-    });
+    }
 
 });
