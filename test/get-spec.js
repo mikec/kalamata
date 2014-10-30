cf765956b852f11a5756a444e3453ca427bbc748
Mike Calvanese <mfcalvanese@gmail.com>
2014-10-30 00:51:31 -0400

    Added before hook error test to get spec

--- test/get-spec.js
+++ test/get-spec.js
@@ -66,4 +66,84 @@ describe('GET request for single item', function() {
 
     });
 
+    describe('with hooks setup', function() {
+
+        var hooks, mockFetchedModel;
+
+        beforeEach(function() {
+            mockFetchedModel = { name: 'mock' };
+            var mockModel = new MockModel('items', {
+                fetch: function() {
+                    return new MockPromise([mockFetchedModel]);
+                }
+            });
+            hooks = {
+                before: function() { },
+                after: function() { },
+                beforeGet: function() { },
+                afterGet: function() { }
+            };
+            spyOn(hooks, 'before');
+            spyOn(hooks, 'after');
+            spyOn(hooks, 'beforeGet');
+            spyOn(hooks, 'afterGet');
+            k.expose(mockModel)
+                .before(hooks.before)
+                .after(hooks.after)
+                .beforeGet(hooks.beforeGet)
+                .afterGet(hooks.afterGet);
+            mockResponse = new MockResponse();
+            mockRequest = new MockRequest({
+                params: { identifier: '1' }
+            });
+            mockApp.getHandlers['/items/:identifier'](mockRequest, mockResponse);
+        });
+
+        it('should call the before hook with the correct arguments',
+        function() {
+            expect(hooks.before.calls.argsFor(0))
+                .toEqual(['1', mockRequest, mockResponse]);
+        });
+
+        it('should call the after hook with the correct arguments',
+        function() {
+            expect(hooks.after.calls.argsFor(0))
+                .toEqual([mockFetchedModel, mockRequest, mockResponse]);
+        });
+
+        it('should call the beforeGet hook with the correct arguments',
+        function() {
+            expect(hooks.beforeGet.calls.argsFor(0))
+                    .toEqual(['1', mockRequest, mockResponse]);
+        });
+
+        it('should call the afterGet hook with the correct arguments',
+        function() {
+            expect(hooks.afterGet.calls.argsFor(0))
+                    .toEqual([mockFetchedModel, mockRequest, mockResponse]);
+        });
+
+    });
+
+    describe('with a before hook that throws an error', function() {
+
+        var hooks, mockFetchedModel;
+
+        beforeEach(function() {
+            hooks = { before: function() { } };
+            spyOn(hooks, 'before').and.throwError('err');
+            k.expose(new MockModel('items')).before(hooks.before);
+            mockResponse = new MockResponse();
+            mockRequest = new MockRequest();
+            spyOn(mockResponse, 'send');
+            mockApp.getHandlers['/items/:identifier'](mockRequest, mockResponse);
+        });
+
+        it('should respond with an error', function() {
+            expect(mockResponse.send.calls.argsFor(0)[0])
+                .toEqual('Error getting items');
+        });
+
+    });
+
 });
